9189a88546a4ee886e2f2399fca6c424f52e585a
Ben England <102834321+ben-england@users.noreply.github.com>
2024-07-04 00:12:17 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import discord
 from discord.ext import commands
+#installed requests using pip
 import requests
+#importing tokens and api keys from another file for safety
 from botconnectioninfo import returntoken
 from api_info import returnapikey
 
 intents = discord.Intents.default()
 intents.members = True
 
+#command prefix
 client = commands.Bot(command_prefix = '!', intents=intents)
 
 @client.event
@@ -14,6 +17,15 @@ async def on_ready():
     print("Bot is online")
     print("----------------------")
 
+@client.command()
+async def summoner(ctx: commands.Context):
+    await ctx.send("Please input the summoner name! Please include the hash to ensure the correct account is loaded")
+    message = await client.wait_for("message", check=lambda msg: msg.author == ctx.author, timeout=60.0)
+    value_input = message.content
+
+    
+
+    
 
 #invoking function from other file
 client.run(returntoken())
