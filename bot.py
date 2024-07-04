ea03b72fab7d7bfa1f72f97288e4d8e2366376b2
Ben England <102834321+ben-england@users.noreply.github.com>
2024-07-04 21:39:42 +0100

    working on debugging api request as not working properly

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import discord
 from discord.ext import commands
 #installed requests using pip
@@ -6,7 +7,8 @@ import requests
 from botconnectioninfo import returntoken
 from api_info import returnapikey
 
-intents = discord.Intents.default()
+
+intents = discord.Intents.all()
 intents.members = True
 
 riot_id_api_url = "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/benji/cring?api_key=RGAPI-a9395dac-f905-4fc3-9b10-d3392a299cce"
@@ -22,19 +24,21 @@ async def on_ready():
 @client.command()
 async def summoner(ctx):
     await ctx.send("Please input the summoner name!")
-    #gamenamemessage = await client.wait_for("message", check=lambda msg: msg.author == ctx.author, timeout=60.0)
-    #gamename_input = gamenamemessage.content
+    gamenamemessage = await client.wait_for("message", check=lambda msg: msg.author == ctx.author, timeout=60.0)
+    gamename_input = gamenamemessage.content
 
-    #await ctx.send ("Please input the tagline! (This is the word/numbers after the hash)")
-    #taglinemessage = await client.wait_for("message", check=lambda msg: msg.author == ctx.author, timeout=60.0)
-    #tagline_input = taglinemessage.content
+    print(gamename_input)
 
-    #async def get_puuid(ctx, gamename_input: str, tagline_input: str):
-        #puuidresponse = requests.get(riot_id_api_url, returnapikey)
-        #if puuidresponse.status_code == 200:
-            #puuiddata = puuidresponse.json()
-            #puuid = puuiddata['puuid']
-            #await ctx.send("The PUUID for {gamename_input}#{tagline_input} is {puuid}")
+    await ctx.send ("Please input the tagline! (This is the word/numbers after the hash)")
+    taglinemessage = await client.wait_for("message", check=lambda msg: msg.author == ctx.author, timeout=60.0)
+    tagline_input = taglinemessage.content
+
+    async def get_puuid(ctx, gamename_input: str, tagline_input: str):
+        puuidresponse = requests.get(riot_id_api_url, returnapikey)
+        if puuidresponse.status_code == 200:
+            puuiddata = puuidresponse.json()
+            puuid = puuiddata['puuid']
+            await ctx.send("The PUUID for {gamename_input}#{tagline_input} is {puuid}")
 
         
 
